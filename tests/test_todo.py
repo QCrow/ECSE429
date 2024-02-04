c00084de97b388292f716c90d90e7ef3b87bb4ec
Kaicheng Wu <kaicheng.wu@mail.mcgill.ca>
2024-02-04 00:24:53 -0500

    more tests...

--- tests/test_todo.py
+++ tests/test_todo.py
@@ -102,3 +102,105 @@ class TestClass:
         patch_response = requests.patch(f"{BASE_URL}/todos/{todo_id}")
         # this should fail as the method is not allowed
         assert patch_response.status_code == 405
+
+    #categories
+    def test_get_categories_by_todo_id(self):
+        todo_id = 1
+        get_response = requests.get(f"{BASE_URL}/todos/{todo_id}/categories")
+        assert get_response.status_code == 200
+
+    def test_create_categories_by_todo_id(self):
+        todo_id = 1
+        category_data = {
+            "id": "1",
+            "title": "categories_test",
+            "description": "test of category 1",
+        }
+        create_response = requests.post(f"{BASE_URL}/todos/{todo_id}/categories", json=category_data)
+        assert create_response.status_code == 201
+
+    def test_update_categories_by_todo_id(self):
+        todo_id = 1
+        category_id = 1
+        category_data = {
+            "id": "1",
+            "title": "categories_test",
+            "description": "test of category 1",
+        }
+        update_response = requests.put(f"{BASE_URL}/todos/{todo_id}/categories", json=category_data)
+        # this should fail as the method is not allowed
+        assert update_response.status_code == 405
+
+    def test_delete_categories_by_todo_id(self):
+        todo_id = 1
+        category_id = 1
+        delete_response = requests.delete(f"{BASE_URL}/todos/{todo_id}/categories")
+        # this should fail as the method is not allowed
+        assert delete_response.status_code == 405
+
+    def test_options_categories_by_todo_id(self):
+        todo_id = 1
+        options_response = requests.options(f"{BASE_URL}/todos/{todo_id}/categories")
+        assert options_response.status_code == 200
+
+    def test_head_categories_by_todo_id(self):
+        todo_id = 1
+        head_response = requests.head(f"{BASE_URL}/todos/{todo_id}/categories")
+        assert head_response.status_code == 200
+
+    def test_patch_categories_by_todo_id(self):
+        todo_id = 1
+        patch_response = requests.patch(f"{BASE_URL}/todos/{todo_id}/categories")
+        # this should fail as the method is not allowed
+        assert patch_response.status_code == 405
+    
+    #tasksof
+    def test_get_tasksof_by_todo_id(self):
+        todo_id = 1
+        get_response = requests.get(f"{BASE_URL}/todos/{todo_id}/tasksof")
+        assert get_response.status_code == 200
+    
+    def test_create_tasksof_by_todo_id(self):
+        todo_id = 1
+        tasksof_data = {
+            "id": "1",
+            "title": "tasksof_test",
+            "description": "test of tasksof 1",
+        }
+        create_response = requests.post(f"{BASE_URL}/todos/{todo_id}/tasksof", json=tasksof_data)
+        assert create_response.status_code == 201
+
+    def test_update_tasksof_by_todo_id(self):
+        todo_id = 1
+        tasksof_id = 1
+        tasksof_data = {
+            "id": "1",
+            "title": "tasksof_test",
+            "description": "test of tasksof 1",
+        }
+        update_response = requests.put(f"{BASE_URL}/todos/{todo_id}/tasksof", json=tasksof_data)
+        # this should fail as the method is not allowed
+        assert update_response.status_code == 405
+
+    def test_delete_tasksof_by_todo_id(self):
+        todo_id = 1
+        tasksof_id = 1
+        delete_response = requests.delete(f"{BASE_URL}/todos/{todo_id}/tasksof")
+        # this should fail as the method is not allowed
+        assert delete_response.status_code == 405
+
+    def test_options_tasksof_by_todo_id(self):
+        todo_id = 1
+        options_response = requests.options(f"{BASE_URL}/todos/{todo_id}/tasksof")
+        assert options_response.status_code == 200
+
+    def test_head_tasksof_by_todo_id(self):
+        todo_id = 1
+        head_response = requests.head(f"{BASE_URL}/todos/{todo_id}/tasksof")
+        assert head_response.status_code == 200
+
+    def test_patch_tasksof_by_todo_id(self):
+        todo_id = 1
+        patch_response = requests.patch(f"{BASE_URL}/todos/{todo_id}/tasksof")
+        # this should fail as the method is not allowed
+        assert patch_response.status_code == 405
